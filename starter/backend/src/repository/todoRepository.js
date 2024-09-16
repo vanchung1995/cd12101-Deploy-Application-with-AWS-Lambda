b99d33628e56ea7b431487261ac2345ace554bf6
Vũ Văn Chung <vanchung1995@gmail.com>
2024-09-16 15:15:30 +0700

    Create todoRepository.js

---
+++ starter/backend/src/repository/todoRepository.js
@@ -0,0 +1,19 @@
+export async function getAll() {
+
+}
+
+export async function get(todoId) {
+    
+}
+
+export async function insert(todoEntity) {
+    
+}
+
+export async function update(todoId, todoEntity) {
+    
+}
+
+export async function remove(todoId) {
+    
+}
