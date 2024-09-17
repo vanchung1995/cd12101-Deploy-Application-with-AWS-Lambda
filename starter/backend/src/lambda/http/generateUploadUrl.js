0664d93fe98b1332cbf348a23afb7c4766a51e4a
Vũ Văn Chung <vanchung1995@gmail.com>
2024-09-17 10:08:50 +0700

    Update generateUploadUrl.js

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,7 +1,46 @@
-export function handler(event) {
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { TodoService } from '../../services/todoService.mjs';
+
+const s3Client = new S3Client()
+const todoService = new TodoService();
+
+export async function handler(event) {
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+  try {
+    await todoService.get(todoId)
+    const url = await getUploadUrl(todoId)
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        newItem: newItem,
+        uploadUrl: url
+      })
+    }
+  } catch (error) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
 }
 
+async function getUploadUrl(todoId) {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: todoId
+  })
+  const url = await getSignedUrl(s3Client, command, {
+    expiresIn: urlExpiration
+  })
+  return url
+}
