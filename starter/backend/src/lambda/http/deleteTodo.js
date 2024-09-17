c7563f4c2c07a5cc59ea62111be5c979a487ef60
Vũ Văn Chung <vanchung1995@gmail.com>
2024-09-17 09:54:27 +0700

    Update deleteTodo.js

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -2,8 +2,14 @@ import { TodoService } from "../../services/todoService.mjs";
 
 const todoService = new TodoService();
 
-export function handler(event) {
+export async function handler(event) {
   const todoId = event.pathParameters.todoId
 
-  return todoService.remove(todoId)
+  await todoService.remove(todoId)
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
 }
