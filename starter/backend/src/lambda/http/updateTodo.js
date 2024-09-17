2c5a0ec10d1f01cfb492a100ebde264339102a47
Vũ Văn Chung <vanchung1995@gmail.com>
2024-09-17 10:09:25 +0700

    Update updateTodo.js

--- starter/backend/src/lambda/http/updateTodo.js
+++ starter/backend/src/lambda/http/updateTodo.js
@@ -5,12 +5,23 @@ const todoService = new TodoService();
 export async function handler(event) {
   const todoId = event.pathParameters.todoId
   const updatedTodo = JSON.parse(event.body)
-  
-  await todoService.update(todoId, updatedTodo)
-  return {
-    statusCode: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
+  try {
+    await todoService.update(todoId, updatedTodo)
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  } catch (error) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
     }
   }
 }
