3a62809cfef43eaf387f11792fc9f0da2b4af341
Vũ Văn Chung <vanchung1995@gmail.com>
2024-09-17 17:16:04 +0700

    Update generateUploadUrl.js

--- starter/backend/src/lambda/http/generateUploadUrl.js
+++ starter/backend/src/lambda/http/generateUploadUrl.js
@@ -1,9 +1,11 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { TodoService } from '../../services/todoService.mjs';
+import { createLogger } from '../../utils/logger.mjs'
 
 const s3Client = new S3Client()
 const todoService = new TodoService();
+const logger = createLogger('todoRepository')
 
 export async function handler(event) {
   const todoId = event.pathParameters.todoId
@@ -23,6 +25,7 @@ export async function handler(event) {
       })
     }
   } catch (error) {
+    logger.error('Error: '+ error)
     return {
       statusCode: 404,
       headers: {
